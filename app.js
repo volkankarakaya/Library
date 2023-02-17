e1d613c9fc90ab8bf6902482d7e6fb6709b9f290
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-02-17 16:21:32 +0300

    refactor update-book function and add remove btn event

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const headerBtn = document.getElementById("header-btn");
 const closeBtn = document.getElementById("close-btn");
 const addBtn = document.getElementById("add-btn");
 
+
 let bookContainer = document.querySelector(".book-container");
 
 headerBtn.addEventListener("click", toggleForm);
 closeBtn.addEventListener("click", toggleForm);
+window.addEventListener('load', ()=>{
+  myLibrary.forEach((item)=>{
+    updateBook(item);
+  })
+});
+
+
+
 
 function toggleForm() {
   document.body.classList.toggle("activeForm");
@@ -56,21 +65,76 @@ function saveBook() {
   addBookToLibrary(bookInstance);
   toggleForm();
   inputs.forEach(clearInput);
-  updateBooks()
+  updateBook(bookInstance);
 }
 
-function updateBooks(){
-  let htmlString = ''
-  for(let i=0; i<myLibrary.length; i++){
-    htmlString += `<div class="book-card">
-    <p>Title: ${myLibrary[i].title}</p>
-    <p>Author: ${myLibrary[i].author}</p>
-    <p>Page: ${myLibrary[i].page}</p>
-    <p>Read: ${myLibrary[i].read}</p>
-    <button class="edit-btn">Edit</button>
-    <button class="remove-btn">Remove</button>
-</div>`
+function updateBook(item){
+  const bookCard = document.createElement('div');
+  bookCard.setAttribute('class','book-card');
+  
+  const titleP = document.createElement('p');
+  const titleText = document.createTextNode('Title: '+item.title);
+  titleP.appendChild(titleText);
 
-  }
-  bookContainer.innerHTML = htmlString;
-}
+  const authorP = document.createElement('p');
+  const authorText = document.createTextNode('Author: '+item.author);
+  authorP.append(authorText);
+
+  const pageP = document.createElement('p');
+  const pageText = document.createTextNode('Page: '+item.page);
+  pageP.appendChild(pageText);
+
+  const readP = document.createElement('p');
+  const readText = document.createTextNode('Read: '+item.read);
+  readP.appendChild(readText);
+
+  const removeBtn = document.createElement('button');
+  const removeText = document.createTextNode('Remove');
+  removeBtn.appendChild(removeText);
+  removeBtn.setAttribute('class','remove-btn')
+
+
+  bookCard.appendChild(titleP);
+  bookCard.appendChild(authorP);
+  bookCard.appendChild(pageP);
+  bookCard.appendChild(readP);
+  bookCard.appendChild(removeBtn);
+
+  bookContainer.appendChild(bookCard);
+
+  removeBtn.addEventListener('click', (e)=>{
+    const parentDiv = e.target.parentElement;
+    parentDiv.remove()
+
+    myLibrary.splice(myLibrary.indexOf(item), 1);
+  })
+
+
+};
+
+// function updateBooks(){
+//   let htmlString = ''
+//   for(let i=0; i<myLibrary.length; i++){
+//     htmlString += `<div class="book-card">
+//     <p>Title: ${myLibrary[i].title}</p>
+//     <p>Author: ${myLibrary[i].author}</p>
+//     <p>Page: ${myLibrary[i].page}</p>
+//     <p>Read: ${myLibrary[i].read}</p>
+//     <button class="edit-btn">Edit</button>
+//     <button class="remove-btn">Remove</button>
+// </div>`
+
+//   }
+//   bookContainer.innerHTML = htmlString;
+// }
+
+
+
+const removeBtns = document.querySelectorAll(".remove-btn");
+removeBtns.forEach((btn)=>{
+  btn.addEventListener('click', (e)=>{
+    let parent = e.parentElement;
+    console.log('clicked')
+  })
+})
+
